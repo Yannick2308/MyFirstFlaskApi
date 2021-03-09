ed8e4972d37c55b22aa00b991e104fdada08731e
Yannick Stepan <yannickstepan@Yannicks-MacBook-Air.fritz.box>
2021-03-09 13:49:24 +0100

    added delete user

--- resources/user.py
+++ resources/user.py
@@ -3,6 +3,7 @@ from models.user import UserModel
 
 
 class UserRegister(Resource):
+
     parser = reqparse.RequestParser()
     parser.add_argument('username',
                         type=str,
@@ -21,7 +22,25 @@ class UserRegister(Resource):
         if UserModel.find_by_username(data['username']):
             return {"message": "A user with that username already exists"}, 400
 
-        user = UserModel(data['username'], data['password'])
+        user = UserModel(data['username'], data['password']) #(**data) geht auch
         user.save_to_db()
 
         return {"message": "User created successfully."}, 201
+
+class User(Resource):
+    @classmethod 
+    def get (cls, user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': 'User not found'}, 404
+        return user.json()
+
+    @classmethod
+    def delete(cls, user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': 'User not found'}, 404
+        user.delete_from_db()
+        return{'message': 'User deleted'}, 200
+
+
